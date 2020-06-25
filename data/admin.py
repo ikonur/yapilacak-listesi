7e3cc5a54a4b0d6298c0ce571995581a19dedb9f
Onur <onurulker321@gmail.com>
2020-06-25 17:42:55 +0300

    turkce karakter sorunu cozuldu

--- data/admin.py
+++ data/admin.py
@@ -5,13 +5,13 @@ from .models import todolist
 
 class todolistadminmodel(admin.ModelAdmin):
 
-	list_display = ('başlık','eklenme_tarihi','durum')
+	list_display = ('baslik','eklenme_tarihi','durum')
 	date_hierarchy = ('eklenme_tarihi')
-	#list_display_links = ('başlık','eklenme_tarihi')
+	#list_display_links = ('baslik','eklenme_tarihi')
 	list_filter = ('eklenme_tarihi','durum')
 	list_editable = ['durum']
 	orering = ['eklenme_tarihi']
-	search_fields = ('başlık','açıklama')
+	search_fields = ('baslik','aciklama')
 	exclude = ['eklenme_tarihi']
 
 	class meta:
--- data/models.py
+++ data/models.py
@@ -6,8 +6,8 @@ from django.utils import timezone
  
 class todolist(models.Model):
 	
-	başlık = models.CharField(max_length=144,verbose_name='başlık')
-	açıklama = models.TextField(blank=True,null=True,verbose_name='açıklama')
+	baslik = models.CharField(max_length=144,verbose_name='başlık')
+	aciklama = models.TextField(blank=True,null=True,verbose_name='açıklama')
 	eklenme_tarihi = models.DateTimeField(default=timezone.now,verbose_name='eklenme_tarihi')
 	durum = models.BooleanField(default=False,verbose_name='yapıldı')
 
@@ -16,4 +16,4 @@ class todolist(models.Model):
 		verbose_name_plural = 'Yapılacaklar'
 
 	def __str__(self):
-		return self.başlık
+		return self.baslik
